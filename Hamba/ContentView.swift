02bb3fc45a108a8047adfb8f5cd18c72123203cf
Thomas Frey <thomas.frey@code.berlin>
2022-02-20 14:38:21 +0100

    Initial Commit

---
+++ Hamba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hamba
+//
+//  Created by Thomas Frey on 20.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hamba/HambaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HambaApp.swift
+//  Hamba
+//
+//  Created by Thomas Frey on 20.02.22.
+//
+
+import SwiftUI
+
+@main
+struct HambaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
