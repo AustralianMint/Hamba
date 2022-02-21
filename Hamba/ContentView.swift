d67f9908acd31e0714c54eb6a1d9219ca0933f5e
Thomas Frey <thomas.frey@code.berlin>
2022-02-21 14:20:16 +0100

    Import MapKit. Declare Map interface in View. Created var specifying what to show on map.

--- Hamba/ContentView.swift
+++ Hamba/ContentView.swift
@@ -5,15 +5,24 @@
 //  Created by Thomas Frey on 20.02.22.
 //
 
+import MapKit
 import SwiftUI
 
-//this comment is testing Github
-
 struct ContentView: View {
+    
+    
+    
+    //Pass var region data to show in "Map" View.
+    //"Map" is embedded map interface
     var body: some View {
-        Text("Hello, Thomas!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
+}
+
+extension ContentView {
+    //Variable containing map data (Long/Lat & Zoom lvl)
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.520008
+                                                                                  , longitude: 13.411000), span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
 }
 
 struct ContentView_Previews: PreviewProvider {
