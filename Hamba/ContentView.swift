7d5e0570c02b70a0a2917fe408e22676330a761a
Thomas Frey <thomas.frey@code.berlin>
2022-02-24 16:46:08 +0100

    Deleted random extension containing map data variable. Placed var in scope

--- Hamba/ContentView.swift
+++ Hamba/ContentView.swift
@@ -9,20 +9,15 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    
-    
-    
+    //variable containing map data, (zoom lvl & Long/Lat)
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.520008
+                                                                                  , longitude: 13.411000), span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
+
     //Pass var region data to show in "Map" View.
     //"Map" is embedded map interface
     var body: some View {
         Map(coordinateRegion: $region)
     }
-}
-
-extension ContentView {
-    //Variable containing map data (Long/Lat & Zoom lvl)
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.520008
-                                                                                  , longitude: 13.411000), span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
 }
 
 struct ContentView_Previews: PreviewProvider {
