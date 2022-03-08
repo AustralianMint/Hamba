a6a7e002a42d2593817b9eac0fe1163d4f2580b2
Thomas Frey <thomas.frey@code.berlin>
2022-03-08 12:07:09 +0100

    Made MapViewModel conform to Objective-C by declaring NSObject. Give MVM access to LocationManagerDelegate. Call location authorisation switch checks onAppear

--- Hamba/ContentView.swift
+++ Hamba/ContentView.swift
@@ -9,29 +9,34 @@ import MapKit
 import SwiftUI
 
 struct MapView: View {
+    
     //Gives MapView (which is a view) access to user location (MapViewModel)
     @StateObject private var viewModel = MapViewModel()
     
-    //variable containing map data, (zoom lvl & Long/Lat)
+    //variable containing initial load map position, (zoom lvl & Long/Lat)
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.520008
                                                                                   , longitude: 13.411000), span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
 
-    //Pass var region data to show in "Map".
+    //binding variable "region", showing it in "Map" (Model View).
     //"Map" is embedded map interface
     var body: some View {
         Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .onAppear {
+                viewModel.checkIfLocationServicesIsEnabled()
+            }
     }
 }
 
 //Just for Xcode Preview sake
-struct ContentView_Previews: PreviewProvider {
+struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
 }
 
-//Handles Location Logic
-final class MapViewModel: ObservableObject {
+//ViewModel which handles Location Logic
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     
     //creating instance of Location Manager Class
     var locationManager: CLLocationManager?
@@ -39,13 +44,14 @@ final class MapViewModel: ObservableObject {
     func checkIfLocationServicesIsEnabled() {
         if CLLocationManager.locationServicesEnabled(){
             locationManager = CLLocationManager()
+            locationManager!.delegate = self
         } else {
             print("Your location services are turned off. Turn them on in Settings")
         }
     }
     
     //INCOMPLETE
-    func checkAppLocationAuthorization() {
+    private func checkAppLocationAuthorization() {
         guard let locationManager = locationManager else { return }
         
         switch locationManager.authorizationStatus {
@@ -61,7 +67,11 @@ final class MapViewModel: ObservableObject {
         @unknown default:
             break
         }
-
+    }
+    
+    //gets called when CLLocationManager Object is created, runs switch checks.
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAppLocationAuthorization()
     }
     
 }
--- Hamba/HambaApp.swift
+++ Hamba/HambaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HambaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MapView()
         }
     }
 }
