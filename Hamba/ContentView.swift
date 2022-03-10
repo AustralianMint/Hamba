b44d80074aa1afc447bfec2b6f4fe5b6f0302fd3
Thomas Frey <thomas.frey@code.berlin>
2022-03-10 12:34:48 +0100

    Moved Map's ViewModel into it's own file. Created Enumeration containing Map details constants (starting point & span). Now I can just reference them.

--- Hamba/ContentView.swift
+++ Hamba/ContentView.swift
@@ -13,8 +13,8 @@ struct MapView: View {
     //Gives MapView (which is a View) access to user location (MapViewModel)
     @StateObject private var viewModel = MapViewModel()
     
-    //binding variable "region", showing it in "Map" (Model View).
-    //"Map" is embedded map interface
+    
+    //referencing MapViewModel to show region data on Map
     var body: some View {
         Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
             .ignoresSafeArea()
@@ -30,48 +30,4 @@ struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
-}
-
-//ViewModel which handles Location Logic
-final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    
-    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.520008
-                                                                                  , longitude: 13.411000), span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
-    
-    //creating instance of Location Manager Class
-    var locationManager: CLLocationManager?
-    
-    func checkIfLocationServicesIsEnabled() {
-        if CLLocationManager.locationServicesEnabled(){
-            locationManager = CLLocationManager()
-            locationManager!.delegate = self
-        } else {
-            print("Your location services are turned off. Turn them on in Settings")
-        }
-    }
-    
-    //INCOMPLETE
-    private func checkAppLocationAuthorization() {
-        guard let locationManager = locationManager else { return }
-        
-        switch locationManager.authorizationStatus {
-            
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            print("Your location is disabled likely due to parental controls")
-        case .denied:
-            print("You have denied this app location permissions. Change this in settings")
-        case .authorizedAlways, .authorizedWhenInUse:
-            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09))
-        @unknown default:
-            break
-        }
-    }
-    
-    //gets called when CLLocationManager Object is created, runs switch checks.
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        checkAppLocationAuthorization()
-    }
-    
 }
---
+++ Hamba/MapViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  MapViewModel.swift
+//  Hamba
+//
+//  Created by Thomas Frey on 09.03.22.
+//
+
+import MapKit
+
+enum MapDetails {
+    static let startingLocation = CLLocationCoordinate2D(latitude: 52.520008, longitude: 13.411000)
+    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09)
+}
+
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: MapDetails.startingLocation, span: MapDetails.defaultSpan)
+    
+    //creating instance of Location Manager Class
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+        } else {
+            print("Your location services are turned off. Turn them on in Settings")
+        }
+    }
+    
+    
+    private func checkAppLocationAuthorization() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is disabled likely due to parental controls")
+        case .denied:
+            print("You have denied this app location permissions. Change this in settings")
+        case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MapDetails.defaultSpan)
+        @unknown default:
+            break
+        }
+    }
+    
+    //gets called when CLLocationManager Object is created, runs switch checks.
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAppLocationAuthorization()
+    }
+    
+}
+
