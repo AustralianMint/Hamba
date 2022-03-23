70cb73ed9111c722a607acf679c60ae445c2e7f1
Thomas Frey <thomas.frey@code.berlin>
2022-03-23 12:57:36 +0100

    Map Markers :)))) Created Structure defining properties & value types for annotations. Populated that with array of Park locations. Referenced these spots in Map(....) with annotation.

--- Hamba/ContentView.swift
+++ Hamba/ContentView.swift
@@ -8,27 +8,45 @@
 import MapKit
 import SwiftUI
 
-//struct is a Value type that encapsulates state & behavior.
+//Defining properties to store values of Park-locations
+struct Spots: Identifiable {
+    let id = UUID()
+    var name = String()
+    var coordinate: CLLocationCoordinate2D
+}
+
+//struct is a 'Value type' that encapsulates state & behavior.
 struct MapView: View {
     
     //Gives MapView (which is a View) access to user location (MapViewModel)
     @StateObject private var viewModel = MapViewModel()
     
+    //Array w/ Park values ('random-access-collection' in map init)
+    let locations = [
+        Spots(name: "James-Simon-Park", coordinate: CLLocationCoordinate2D(latitude: 52.521927, longitude: 13.399313)),
+        Spots(name: "Waldeck-PakrTrue", coordinate: CLLocationCoordinate2D(latitude: 52.506322, longitude: 13.403756)),
+        Spots(name: "Luisenstädtischer Kirchpark", coordinate: CLLocationCoordinate2D(latitude: 52.508725, longitude: 13.407724))
+    ]
     
-    //referencing MapViewModel to show region data on Map
+    
+    //initializing a Map in a View w/ necessary arguments
     var body: some View {
-        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
-            .ignoresSafeArea()
-            .accentColor(.orange)
-            .onAppear {
+        Map(coordinateRegion: $viewModel.region,
+            showsUserLocation: true,
+            annotationItems: locations,
+            annotationContent: {
+            location in MapMarker(coordinate: location.coordinate,
+                                  tint: Color.purple)
+        }).ignoresSafeArea()
+            .onAppear{
                 viewModel.checkIfLocationServicesIsEnabled()
             }
-    }
 }
 
 //Just for Xcode Preview sake
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
+    }
     }
 }
