7c1c16e8d5323239fa3994a55d3b581712dfe31a
Thomas Frey <thomas.frey@code.berlin>
2022-03-27 22:39:17 +0200

    Refactor: Replaced Annotation with correct pre-built view 'Circle()'. Declared Park Location Array as '@State private var'... Idk quite why yet tho.

--- Hamba/ContentView.swift
+++ Hamba/ContentView.swift
@@ -22,12 +22,13 @@ struct MapView: View {
     @StateObject private var viewModel = MapViewModel()
     
     //Array w/ Park values ('random-access-collection' in map init)
-    let locations = [
+    @State private var locations: [Spots] = [
         Spots(name: "James-Simon-Park", coordinate: CLLocationCoordinate2D(latitude: 52.521927, longitude: 13.399313)),
         Spots(name: "Waldeck-PakrTrue", coordinate: CLLocationCoordinate2D(latitude: 52.506322, longitude: 13.403756)),
-        Spots(name: "Luisenstädtischer Kirchpark", coordinate: CLLocationCoordinate2D(latitude: 52.508725, longitude: 13.407724))
+        Spots(name: "Luisenstädtischer Kirchpark", coordinate: CLLocationCoordinate2D(latitude: 52.508725, longitude: 13.407724))    
     ]
     
+    @State private var pointOfInteres: MKPointOfInterestFilter = .excludingAll
     
     //initializing a Map in a View w/ necessary arguments
     var body: some View {
@@ -35,18 +36,22 @@ struct MapView: View {
             showsUserLocation: true,
             annotationItems: locations,
             annotationContent: {
-            location in MapMarker(coordinate: location.coordinate,
-                                  tint: Color.purple)
+            location in MapAnnotation(coordinate: location.coordinate) {
+                Circle()
+                    .stroke(.red, lineWidth: 5.0)
+                    .frame(width: 30, height: 30)
+                    .blur(radius: 0.3)
+            }
         }).ignoresSafeArea()
             .onAppear{
                 viewModel.checkIfLocationServicesIsEnabled()
             }
-}
+    }
 
 //Just for Xcode Preview sake
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
-    }
+        }
     }
 }
