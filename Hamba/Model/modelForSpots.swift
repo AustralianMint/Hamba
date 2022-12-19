5cb26ed69c6f27a2bfa65c94443416c33dfe4d34
Thomas Frey <thomas.frey@code.berlin>
2022-12-19 11:27:46 +0100

    add ten spots

--- Hamba/Model/modelForSpots.swift
+++ Hamba/Model/modelForSpots.swift
@@ -19,7 +19,7 @@ struct Spots: Identifiable {
 
 var locations: [Spots] = [
     Spots(name: "James-Simon-Park", coordinate: CLLocationCoordinate2D(latitude: 52.521927, longitude: 13.399313)),
-    Spots(name: "Waldeck-PakrTrue", coordinate: CLLocationCoordinate2D(latitude: 52.506322, longitude: 13.403756)),
+    Spots(name: "Waldeck-PakrTrue", coordinate: CLLocationCoordinate2D(latitude: 52.50624, longitude: 13.40340)),
     Spots(name: "Luisenstädtischer Kirchpark", coordinate: CLLocationCoordinate2D(latitude: 52.508725, longitude: 13.407724)),
     Spots(name: "Märkischer Spot am Water", coordinate: CLLocationCoordinate2D(latitude: 52.514207, longitude: 13.413992)),
     Spots(name: "Osloer North - Bench Spot", coordinate: CLLocationCoordinate2D(latitude: 52.55672, longitude: 13.38133)),
@@ -30,5 +30,14 @@ var locations: [Spots] = [
     Spots(name: "Long times", coordinate: CLLocationCoordinate2D(latitude: 52.49535, longitude: 13.44796)),
     Spots(name: "Lohmühlenspot", coordinate: CLLocationCoordinate2D(latitude: 52.49499, longitude: 13.44643)),
     Spots(name: "Overview", coordinate: CLLocationCoordinate2D(latitude: 52.49433, longitude: 13.44299)),
-    Spots(name: "Good when full", coordinate: CLLocationCoordinate2D(latitude: 52.49204, longitude: 13.46932))
+    Spots(name: "Good when full", coordinate: CLLocationCoordinate2D(latitude: 52.49204, longitude: 13.46932)),
+    Spots(name: "May the lord be open", coordinate: CLLocationCoordinate2D(latitude: 52.53674, longitude: 13.41829)),
+    Spots(name: "TBD", coordinate: CLLocationCoordinate2D(latitude: 52.55859, longitude: 13.35737)),
+    Spots(name: "Vosters' Ufer", coordinate: CLLocationCoordinate2D(latitude: 52.52190, longitude: 13.36933)),
+    Spots(name: "Welcome to B", coordinate: CLLocationCoordinate2D(latitude: 52.52289, longitude: 13.36967)),
+    Spots(name: "Grab shell dude", coordinate: CLLocationCoordinate2D(latitude: 52.43854, longitude: 13.17658)),
+    Spots(name: "Boiz", coordinate: CLLocationCoordinate2D(latitude: 52.49611, longitude: 13.40912)),
+    Spots(name: "TBD", coordinate: CLLocationCoordinate2D(latitude: 52.57264, longitude: 13.36183)),
+    Spots(name: "Schäfer Spot", coordinate: CLLocationCoordinate2D(latitude: 52.56517, longitude: 13.35932)),
+    Spots(name: "MUC", coordinate: CLLocationCoordinate2D(latitude: 48.15685, longitude: 11.58068))
 ]
