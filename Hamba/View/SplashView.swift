6432124ae840f3b36506d02ddd4841fc94a8792e
AustralianMint <thomas.frey@code.berlin>
2023-01-15 10:17:00 +0200

    add splashScreen imgs

---
+++ Hamba/View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Hamba
+//
+//  Created by Thomas Frey on 15.01.23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
