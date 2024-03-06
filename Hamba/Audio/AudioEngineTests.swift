4b351d3f6be76b8c70f0615a329b1021a16c356d
peterSanyo <peter.sanyo@code.berlin>
2024-03-06 17:56:56 +0100

    audioTesting-added testPulsatingReverbEffect

--- Hamba/Audio/AudioEngineTests.swift
+++ Hamba/Audio/AudioEngineTests.swift
@@ -11,14 +11,14 @@ import SwiftUI
 import XCTest
 
 final class AudioEngineTests: XCTestCase {
-    private lazy var testAudioEngine = AudioEngine()
+    private lazy var mockAudioEngine = AudioEngine()
 
     func testAudioEngineIsSetup() {
-        testAudioEngine.setupAudioEngine()
-        let volume = testAudioEngine.audioEngine.mainMixerNode.outputVolume
+        mockAudioEngine.setupAudioEngine()
+        let volume = mockAudioEngine.audioEngine.mainMixerNode.outputVolume
 
-        XCTAssertFalse(testAudioEngine.audioPlayerNode.isPlaying)
-        XCTAssertTrue(testAudioEngine.audioEngine.isRunning)
+        XCTAssertFalse(mockAudioEngine.audioPlayerNode.isPlaying)
+        XCTAssertTrue(mockAudioEngine.audioEngine.isRunning)
         XCTAssertEqual(volume, 0)
     }
 
@@ -27,19 +27,19 @@ final class AudioEngineTests: XCTestCase {
         let expectationMiddle = XCTestExpectation(description: "Middle of fade")
         let expectationEnd = XCTestExpectation(description: "End of fade")
 
-        testAudioEngine.setupAudioEngine()
-        testAudioEngine.fade(up: true, in: fadeTime)
+        mockAudioEngine.setupAudioEngine()
+        mockAudioEngine.fade(up: true, in: fadeTime)
 
         /// middle of fade
         DispatchQueue.main.asyncAfter(deadline: .now() + fadeTime / 2) {
-            let midVolume = self.testAudioEngine.audioEngine.mainMixerNode.outputVolume
+            let midVolume = self.mockAudioEngine.audioEngine.mainMixerNode.outputVolume
             XCTAssertEqual(midVolume, 0.5, accuracy: 0.05)
             expectationMiddle.fulfill()
         }
 
         /// End of fade
         DispatchQueue.main.asyncAfter(deadline: .now() + fadeTime) {
-            let endVolume = self.testAudioEngine.audioEngine.mainMixerNode.outputVolume
+            let endVolume = self.mockAudioEngine.audioEngine.mainMixerNode.outputVolume
             XCTAssertEqual(endVolume, 1, accuracy: 0.05)
             expectationEnd.fulfill()
         }
@@ -48,35 +48,61 @@ final class AudioEngineTests: XCTestCase {
     }
 
     func testPlaySound() {
-        testAudioEngine.setupAudioEngine()
-        testAudioEngine.playSound(file: .focusLoopCorporateMusic)
+        mockAudioEngine.setupAudioEngine()
+        mockAudioEngine.playSound(file: .focusLoopCorporateMusic)
 
-        XCTAssertTrue(testAudioEngine.audioPlayerNode.isPlaying)
+        XCTAssertTrue(mockAudioEngine.audioPlayerNode.isPlaying)
     }
-    
+
     func testPauseSound() {
-        testAudioEngine.setupAudioEngine()
-        testAudioEngine.playSound(file: .focusLoopCorporateMusic)
-        testAudioEngine.pauseSound(in: 1)
+        mockAudioEngine.setupAudioEngine()
+        mockAudioEngine.playSound(file: .focusLoopCorporateMusic)
+        mockAudioEngine.pauseSound(in: 1)
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            XCTAssertEqual(self.testAudioEngine.audioPlayerNode.volume, 0)
-            XCTAssertFalse(self.testAudioEngine.audioPlayerNode.isPlaying)
+            XCTAssertEqual(self.mockAudioEngine.audioPlayerNode.volume, 0)
+            XCTAssertFalse(self.mockAudioEngine.audioPlayerNode.isPlaying)
         }
     }
-    
+
     func testResumeSound() {
-        testAudioEngine.setupAudioEngine()
-        testAudioEngine.playSound(file: .focusLoopCorporateMusic)
-        testAudioEngine.pauseSound(in: 1)
-        testAudioEngine.resumeSound(in: 1)
+        mockAudioEngine.setupAudioEngine()
+        mockAudioEngine.playSound(file: .focusLoopCorporateMusic)
+        mockAudioEngine.pauseSound(in: 1)
+        mockAudioEngine.resumeSound(in: 1)
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            XCTAssertEqual(self.testAudioEngine.audioPlayerNode.volume, 1)
-            XCTAssertTrue(self.testAudioEngine.audioPlayerNode.isPlaying)
+            XCTAssertEqual(self.mockAudioEngine.audioPlayerNode.volume, 1)
+            XCTAssertTrue(self.mockAudioEngine.audioPlayerNode.isPlaying)
         }
     }
-    
-    
-    
+
+    func testPulsatingReverbEffect() {
+        let reverbTimer: Double = 10
+        let shouldReverbPercentage: Float = 80
+        let initialReverbPercentage = mockAudioEngine.reverbNode.wetDryMix
+
+        let expectationMiddle = XCTestExpectation(description: "Middle of fade")
+        let expectationEnd = XCTestExpectation(description: "End of fade")
+
+        mockAudioEngine.setupAudioEngine()
+        mockAudioEngine.firstFadeIn(audioFile: .focusLoopCorporateMusic, fadeDuration: 1)
+
+        XCTAssertTrue(initialReverbPercentage == 0)
+
+        mockAudioEngine.pulsatingReverbEffect(in: reverbTimer)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + reverbTimer / 2) {
+            let midReverbPercentage = self.mockAudioEngine.reverbNode.wetDryMix
+            XCTAssertEqual(midReverbPercentage, shouldReverbPercentage / 2, accuracy: 5)
+            expectationMiddle.fulfill()
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + reverbTimer) {
+            let maxReverbPercentage = self.mockAudioEngine.reverbNode.wetDryMix
+            XCTAssertEqual(maxReverbPercentage, shouldReverbPercentage, accuracy: 5)
+            expectationEnd.fulfill()
+        }
+        wait(for: [expectationMiddle, expectationEnd], timeout: reverbTimer + 1)
+    }
 }
