55229bcbc2310e2bc2f42549344dadbaf17c224b
Thomas Frey <thomas.frey@code.berlin>
2024-03-16 02:22:52 +0100

    add TabView to display swipable pictures.
    "

--- Hamba/View/MapView/mainMap.swift
+++ Hamba/View/MapView/mainMap.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct mainMap: View {
     @ObservedObject var mapViewModel: MapViewModel
-
+    var images = ["birbIsTheWirb", "B-day", "Boiz"]
+    
     var body: some View {
         ZStack {
             Map(coordinateRegion: $mapViewModel.region,
@@ -19,21 +20,18 @@ struct mainMap: View {
             { location in
                 MapAnnotation(coordinate: location.coordinate) {
                     NavigationLink {
-                        Image(location.spotImage)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .scaleEffect(1 + mapViewModel.currentAmmount)
-                            .gesture(
-                                MagnificationGesture()
-                                    .onChanged { value in
-                                        mapViewModel.currentAmmount = value - 1
-                                    }
-                                    .onEnded { _ in
-                                        withAnimation(.default) {
-                                            mapViewModel.currentAmmount = 0
-                                        }
-                                    }
-                            )
+                        TabView {
+                            ForEach(location.spotImage, id: \.self) { image in
+                                ZStack {
+                                    Image(image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                }
+                            }
+                        }
+                        .tabViewStyle(.page)
+                        .indexViewStyle(.page(backgroundDisplayMode: .never))
+
                         Text(location.name)
                             .bold()
                             .font(.system(size: 27, weight: .heavy, design: .rounded))
@@ -56,3 +54,24 @@ struct mainMap: View {
 #Preview {
     mainMap(mapViewModel: MapViewModel())
 }
+
+/*
+ This is how i previously displayed an image.
+ I wanna keep this in case i need the zoom ability
+ 
+                        Image(location.spotImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaleEffect(1 + mapViewModel.currentAmmount)
+                            .gesture(
+                                MagnificationGesture()
+                                    .onChanged { value in
+                                        mapViewModel.currentAmmount = value - 1
+                                    }
+                                    .onEnded { _ in
+                                        withAnimation(.default) {
+                                            mapViewModel.currentAmmount = 0
+                                        }
+                                    }
+                            )
+*/
